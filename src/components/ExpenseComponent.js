b07ba8bb3765467db61d78bb432484e7d7b47226
prashantsingh9079 <prashant.singh9079@gmail.com>
2023-07-12 11:59:28 +0530

    integrating firebase realtime db

--- src/components/ExpenseComponent.js
+++ src/components/ExpenseComponent.js
@@ -1,18 +1,42 @@
-import React,{useRef, useState} from 'react'
-import { Card, Button, Nav, Table} from 'react-bootstrap'
+import React, { useRef, useState, useEffect } from 'react'
+import { Card, Button, Nav, Table } from 'react-bootstrap'
+import axios from 'axios'
 
 export default function ExpenseComponent() {
     const descRef = useRef();
     const amountRef = useRef();
     const categoryRef = useRef();
-    const [arr,setArr]=useState([])
+    const [arr, setArr] = useState([])
+    useEffect(() => {
+        async function fetchData() {
+            
+            const res = await axios.get("https://expensetracker-deb9a-default-rtdb.firebaseio.com/expense.json")
+            console.log(res)
+            if (res.status === 200) {
+                setArr((p) => {
+                    let na = []
+                    for (let i in res.data) {
+                        na.push(res.data[i])
+                    }
 
-    function submitHandler(e)
-    {
+                    return na;
+                })
+            }
+            else
+            {
+                alert("something went wrong in loading previous data !!!")
+            }
+
+        }
+        fetchData()
+    }, [])
+
+    function submitHandler(e) {
         e.preventDefault();
-        const formData = {desc:descRef.current.value,amount:amountRef.current.value, category:categoryRef.current.value};
+        const formData = { desc: descRef.current.value, amount: amountRef.current.value, category: categoryRef.current.value };
+        axios.post("https://expensetracker-deb9a-default-rtdb.firebaseio.com/expense.json", formData)
         console.log(formData)
-        setArr((p)=>{
+        setArr((p) => {
             let newArr = [...p];
             newArr.push(formData)
             return newArr
@@ -29,7 +53,7 @@ export default function ExpenseComponent() {
             </Nav>
 
             <Card style={{ margin: '10rem', padding: '3rem', background: 'skyblue', }}>
-                <label  htmlFor="desc"><i>Item Description</i></label>
+                <label htmlFor="desc"><i>Item Description</i></label>
                 <input id='desc' type="text" placeholder='Expense Desciption' ref={descRef} />
                 <br /><br />
                 <label htmlFor="amount"><i>Item Amount</i></label>
@@ -46,31 +70,30 @@ export default function ExpenseComponent() {
             </Card>
 
 
-            {arr.length>0 && 
-            <div style={{margin:'5rem'}}>
-            <Table  striped bordered hover variant="dark">
-            <thead>
-              <tr>
-                <th>Item Description</th>
-                <th>Item Amount</th>
-                <th>Item Category</th>
-              </tr>
-            </thead>
-            <tbody>
-                {arr.map((i) =>
-                {
-                    return (
-                        <tr>
-                            <td>{i.desc}</td>
-                            <td>{i.amount}</td>
-                            <td>{i.category}</td>
-                        </tr>
-                    )
-                }
-                )}
-            </tbody>
-          </Table>
-          </div>
+            {arr.length > 0 &&
+                <div style={{ margin: '5rem' }}>
+                    <Table striped bordered hover variant="dark">
+                        <thead>
+                            <tr>
+                                <th>Item Description</th>
+                                <th>Item Amount</th>
+                                <th>Item Category</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {arr.map((i) => {
+                                return (
+                                    <tr key={i.desc}>
+                                        <td>{i.desc}</td>
+                                        <td>{i.amount}</td>
+                                        <td>{i.category}</td>
+                                    </tr>
+                                )
+                            }
+                            )}
+                        </tbody>
+                    </Table>
+                </div>
             }
         </>
     )
